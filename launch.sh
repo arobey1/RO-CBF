dc633e272b0f5d764930184b1b3a25bd5cf299a3
Alex Robey <arobey1@seas.upenn.edu>
2021-05-17 14:46:56 -0400

    updated launcher

--- launch.sh
+++ launch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-### CHECK CTE *= 2 actually worked
-
 export SYSTEM='carla'
-export DATA_PATH_1='./data/carla/Data_Collection_Compiled_noisy3.pd'
-export DATA_PATH_2='./data/carla/Data_Collection_Compiled_noisy4.pd'
+export DATA_PATH_1='./data/carla/Data_Collection_Compiled_noisy5.pd'
+export DATA_PATH_2='./data/carla/Data_Collection_Compiled_noisy6.pd'
+export DATA_PATH_3='./data/carla/Data_Collection_Compiled_noisy7.pd'
+export DATA_PATH_4='./data/carla/Data_Collection_Compiled_noisy8.pd'
 
-export RESULTS_PATH='./results-clean-data-aug-fewer-safe-states'
+export RESULTS_PATH='./results-less-conservative-larger-delta-data-aug'
 
 # Margins for optimization constraints 
 export GAMMA_SAFE=0.3
@@ -19,6 +19,8 @@ export LAMBDA_PARAM=0.01
 
 # Robustness
 export LAMBDA_ROBUST=0.6
+export DELTA_F=1.0
+export DELTA_G=1.0
 
 # Lipschitz output term
 export LIP_OUTPUT_TERM=0.1
@@ -44,7 +46,7 @@ export DUAL_SCHEME='ae'
 # export MIN_N_NEIGHBORS=200
 
 # For clean data with data aug
-export NEIGHBOR_THRESH=0.07
+export NEIGHBOR_THRESH=0.008
 export MIN_N_NEIGHBORS=200
 
 # Additional state sampling
@@ -54,8 +56,9 @@ export N_SAMP_ALL=0
 
 # $DATA_PATH_2 $DATA_PATH_3
 
+export CUDA_VISIBLE_DEVICES=3
 python main.py \
-    --system $SYSTEM --data-path $DATA_PATH_1 $DATA_PATH_2 --results-path $RESULTS_PATH \
+    --system $SYSTEM --data-path $DATA_PATH_1 $DATA_PATH_2 $DATA_PATH_3 $DATA_PATH_4 --results-path $RESULTS_PATH \
     --gamma-safe $GAMMA_SAFE --gamma-unsafe $GAMMA_UNSAFE --gamma-dyn $GAMMA_DYN \
     --lambda-grad $LAMBDA_GRAD --lambda-param $LAMBDA_PARAM \
     --net-dims ${NET_DIMS[@]} --n-epochs $N_EPOCHS \
@@ -63,5 +66,5 @@ python main.py \
     --nbr-thresh $NEIGHBOR_THRESH --min-n-nbrs $MIN_N_NEIGHBORS \
     --n-samp-unsafe $N_SAMP_UNSAFE --n-samp-safe $N_SAMP_SAFE --n-samp-all $N_SAMP_ALL \
     --dual-scheme $DUAL_SCHEME \
-    --robust --lambda-robust $LAMBDA_ROBUST
+    --robust --lambda-robust $LAMBDA_ROBUST --delta-f $DELTA_F --delta-g $DELTA_G --data-augmentation
     # --use-lip-output-term --lip-output-term $LIP_OUTPUT_TERM
