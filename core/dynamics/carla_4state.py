3818fa06d7385eb6429527476f850f5502773cfb
Alex Robey <arobey1@seas.upenn.edu>
2021-06-30 10:55:38 -0400

    added normalization in dynamics

--- core/dynamics/carla_4state.py
+++ core/dynamics/carla_4state.py
@@ -1,9 +1,11 @@
 import jax.numpy as jnp
 
 class CarlaDynamics:
-    def __init__(self):
+    def __init__(self, T_x):
         self._state_dim = 4
         self._input_dim = 1
+        self._Tx = T_x
+        self._inv_Tx = jnp.linalg.inv(self._Tx)
 
     @property
     def state_dim(self):
@@ -14,17 +16,21 @@ class CarlaDynamics:
         return self._input_dim
 
     def f(self, state, disturbance):
-        c, v, θ_e, d = state
+        unnormalized_state = self._inv_Tx @ state.reshape(self._state_dim, 1)
+        c, v, θ_e, d = unnormalized_state.reshape(self._state_dim,)
         ϕ_dot_t = disturbance
-        return jnp.array([
+        new_state = jnp.array([
             v * jnp.sin(θ_e), 
             -1.0954 * v - 0.007 * v ** 2 - 0.1521 * d + 3.37387,
             -ϕ_dot_t,
             3.6 * v - 20
         ]).reshape(self._state_dim, 1)
+        return self._Tx @ new_state
 
     def g(self, state):
-        c, v, θ, d = state
-        return jnp.array([
+        unnormalized_state = self._inv_Tx @ state.reshape(self._state_dim, 1)
+        c, v, θ, d = unnormalized_state.reshape(self._state_dim,)
+        new_state = jnp.array([
             0., 0., v / 2.51, 0.
         ]).reshape(self._state_dim, 1)
+        return self._Tx @ new_state
