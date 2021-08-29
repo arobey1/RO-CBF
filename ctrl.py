8c6fe88811d467fcdc13f0ab369f6f0bb99a26a6
Lejun Jiang <lejunj@umich.edu>
2021-08-28 22:44:20 -0400

    add and modify codes for interfacing with carla

--- ctrl.py
+++ ctrl.py
@@ -8,10 +8,11 @@ import os
 
 from core.dynamics.carla_4state import CarlaDynamics
 
-ROOT = 'results-left-turn-normalized'
+ROOT = 'old_trained_results/0720/results-less-robust-low-margins-all-data/'
 CBF_PATH = os.path.join(ROOT, 'trained_cbf.npy')
 ARGS_PATH = os.path.join(ROOT, 'args.json')
 META_DATA_PATH = os.path.join(ROOT, 'meta_data.json')
+
 
 def main():
 
@@ -22,7 +23,7 @@ def main():
     with open(CBF_PATH, 'rb') as handle:
         loaded_params = pickle.load(handle)
 
-    learned_h = lambda x: jnp.sum(net.apply(loaded_params, x))
+    def learned_h(x): return jnp.sum(net.apply(loaded_params, x))
     zero_ctrl = get_zero_controller()
 
     safe_ctrl = make_safe_controller(zero_ctrl, learned_h, args_dict, meta_data)
@@ -30,11 +31,12 @@ def main():
     # Example usage:
     u = safe_ctrl(jnp.array([1., 2., 3., 4.]), 1)
 
+
 def make_safe_controller(nominal_ctrl, h, args_dict, meta_data):
     """Create a safe controller using learned hybrid CBF."""
 
     delta_f, delta_g = args_dict['delta_f'], args_dict['delta_g']
-    maxes = meta_data['normalizers']
+    # maxes = meta_data['normalizers']
     # T_x = jnp.diag(jnp.array([
     #     maxes['cte'], maxes['speed'], maxes['theta_e'], maxes['d']
     # ]))
@@ -42,14 +44,14 @@ def make_safe_controller(nominal_ctrl, h, args_dict, meta_data):
 
     dh = jax.grad(h, argnums=0)
     dyn = CarlaDynamics(T_x)
-    alpha = lambda x : x
-    norm = lambda x : jnp.linalg.norm(x)
-    cpnorm = lambda x : cp.norm(x)
-    dot = lambda x, y : jnp.dot(x, y)
+    def alpha(x): return x
+    def norm(x): return jnp.linalg.norm(x)
+    def cpnorm(x): return cp.norm(x)
+    def dot(x, y): return jnp.dot(x, y)
 
     def safe_ctrl(x, d):
         """Solves HCBF-QP to map an input state to a safe action u.
-        
+
         Params:
             x: state.
             d: disturbance.
@@ -57,9 +59,9 @@ def make_safe_controller(nominal_ctrl, h, args_dict, meta_data):
 
         cte, v, θ_e, d_var = x
         # x = jnp.array([
-        #     cte / float(maxes['cte']), 
-        #     v / float(maxes['speed']), 
-        #     θ_e / float(maxes['theta_e']), 
+        #     cte / float(maxes['cte']),
+        #     v / float(maxes['speed']),
+        #     θ_e / float(maxes['theta_e']),
         #     d_var / float(maxes['d'])
         # ]).reshape(4,)
 
@@ -70,17 +72,19 @@ def make_safe_controller(nominal_ctrl, h, args_dict, meta_data):
         u_mod = cp.Variable(len(u_nom))
         obj = cp.Minimize(cp.sum_squares(u_mod - u_nom))
         constraints = [
-            dot(dh(x), dyn.f(x, d)) + u_mod.T @ dot(dyn.g(x).T, dh(x)) + alpha(h(x)) - norm(dh(x)) * (delta_f + delta_g * cpnorm(u_mod)) >= 0
+            dot(dh(x), dyn.f(x, d)) + u_mod.T @ dot(dyn.g(x).T, dh(x)) +
+            alpha(h(x)) - norm(dh(x)) * (delta_f + delta_g * cpnorm(u_mod)) >= 0
         ]
-        
+
         prob = cp.Problem(obj, constraints)
-        prob.solve(solver=cp.SCS, verbose=True, max_iters=20000, eps=1e-10)
+        prob.solve(solver=cp.SCS, verbose=False, max_iters=20000, eps=1e-10)
 
         if prob.status in (cp.OPTIMAL, cp.OPTIMAL_INACCURATE):
-            return u_mod.value
-        return jnp.array([0.])
+            return u_mod.value, h(x), dh(x), (prob.status in (cp.OPTIMAL, cp.OPTIMAL_INACCURATE))
+        return jnp.array([0.]), h(x), dh(x), (prob.status in (cp.OPTIMAL, cp.OPTIMAL_INACCURATE))
 
     return safe_ctrl
+
 
 def net_fn(net_dims=[32, 16]):
     """Feed-forward NN architecture."""
@@ -92,15 +96,18 @@ def net_fn(net_dims=[32, 16]):
 
     return hk.Sequential(layers)
 
+
 def get_zero_controller():
     """Returns a zero controller"""
 
     return lambda state: jnp.array([0.])
 
+
 def load_json(fname):
     with open(fname) as json_file:
         data = json.load(json_file)
     return data
+
 
 if __name__ == '__main__':
     main()
