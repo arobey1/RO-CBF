e8cd79a1201dc3e59314c9b4043bec089bf8e9ab
Lejun Jiang <lejunj@umich.edu>
2021-11-04 01:18:29 -0400

    update script for server

--- carla/automatic_control_state_based_cbf_train_location_server_version.py
+++ carla/automatic_control_state_based_cbf_train_location_server_version.py
@@ -49,9 +49,10 @@ try:
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
 except IndexError:
+    print("now")
     pass
 
-sys.path.append(glob.glob('../../../carla/dist/carla-0.9.9-py3.7-linux-x86_64.egg')[0])
+# sys.path.append(glob.glob('../../../carla/dist/carla-0.9.9-py3.7-linux-x86_64.egg')[0])
 
 # ==============================================================================
 # -- Add PythonAPI for release mode --------------------------------------------
